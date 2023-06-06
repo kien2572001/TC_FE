0aaec5f86c53d54169be399f035494fb93b07fa9
ldhoang201 <hoangithub147@gmail.com>
2023-06-06 15:04:04 +0700

    fix app layout

--- src/app/common/Header.tsx
+++ src/app/common/Header.tsx
@@ -30,17 +30,23 @@ export default function Header() {
     return (
         <div className="container">
             <div className="navbar bg-base-100 rounded-none">
-                <div className="flex-1 ml-20">
-                    <Link href={'/'} className="btn btn-ghost normal-case text-xl">TC</Link>
-                </div>
-                <div className="flex-1 gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="検索.." className="input input-bordered w-24 md:w-auto" id="search-bar" onKeyUp={handleKeyPress} />
-                    </div>
-                    <button className="btn-search btn btn-ghost btn-circle" onClick={handleSearch}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                    </button>
-                </div>
+                {
+                    localStorage.getItem('accessToken') && (
+                        <div>
+                            <div className="flex-1 ml-20">
+                                <Link href={'/'} className="btn btn-ghost normal-case text-xl">TC</Link>
+                            </div>
+                            <div className="flex-1 gap-2">
+                                <div className="form-control">
+                                    <input type="text" placeholder="検索.." className="input input-bordered w-24 md:w-auto" id="search-bar" onKeyUp={handleKeyPress} />
+                                </div>
+                                <button className="btn-search btn btn-ghost btn-circle" onClick={handleSearch}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
                 {
                     localStorage.getItem('accessToken') ? (
                         <div>
