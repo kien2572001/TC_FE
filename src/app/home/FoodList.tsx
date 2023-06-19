e22865729cad29ad9e5178f847aa98c59dcacba8
ldhoang201 <hoangithub147@gmail.com>
2023-06-19 20:54:00 +0700

    fix not-found text

--- src/app/home/FoodList.tsx
+++ src/app/home/FoodList.tsx
@@ -23,14 +23,14 @@ const FoodList = ({ foodsData }: { foodsData: any }) => {
   };
 
   if (!foods || foods.length === 0) {
-    return <p>No foods found.</p>;
+    return <p>料理が見つかりませんでした.</p>;
   }
 
 
   return (
     <div className="flex overflow-x-scroll pt-[10px] pb-[20px]">
       {foods.map((food) => (
-        
+
         // <div
         //   key={food.id}
         //   className="flex m-auto flex-col border-[1px] border-black border-solid p-[10px] mx-[30px] my-[20px]"
@@ -44,28 +44,28 @@ const FoodList = ({ foodsData }: { foodsData: any }) => {
         //   </div>
         // </div>
         <div
-                className="ml-4 mt-4 relative hover:scale-105 transition-all duration-300 flex-[1_0_20%] "
-                key={food.id}
-                onClick={() => navigateToFoodDetail(food.id)}
-              >
-                <div className="absolute top-5 left-5 bg-[#FF903F] text-white font-bold text-xs p-2 z-20 rounded ">
-                  <StarFilled /> {Number.parseFloat(food.rating).toFixed(1)}
-                </div>
-                <div className="p-[10px] h-[270px] max-w-[200px] text-gray-700 transition-shadow duration-300 shadow-sm bg-white relative mx-auto  overflow-hidden  w-full cursor-pointer rounded-md border border-orange-200 border-solid">
-                  {/* Nội dung */}
-                  <div className="h-[170px] w-full overflow-hidden">
-                    <img
-                      src={food.photoUrl}
-                      alt="food"
-                      className="w-full h-fit"
-                    />
-                  </div>
-                  <h5 className="font-bold my-2 ">{food.name}</h5>
-                  <p className="text-[#FF7918] font-bold text-base mt-2">
-                    {food.price}VND
-                  </p>
-                </div>
-              </div>
+          className="ml-4 mt-4 relative hover:scale-105 transition-all duration-300 flex-[1_0_20%] "
+          key={food.id}
+          onClick={() => navigateToFoodDetail(food.id)}
+        >
+          <div className="absolute top-5 left-5 bg-[#FF903F] text-white font-bold text-xs p-2 z-20 rounded ">
+            <StarFilled /> {Number.parseFloat(food.rating).toFixed(1)}
+          </div>
+          <div className="p-[10px] h-[270px] max-w-[200px] text-gray-700 transition-shadow duration-300 shadow-sm bg-white relative mx-auto  overflow-hidden  w-full cursor-pointer rounded-md border border-orange-200 border-solid">
+            {/* Nội dung */}
+            <div className="h-[170px] w-full overflow-hidden">
+              <img
+                src={food.photoUrl}
+                alt="food"
+                className="w-full h-fit"
+              />
+            </div>
+            <h5 className="font-bold my-2 ">{food.name}</h5>
+            <p className="text-[#FF7918] font-bold text-base mt-2">
+              {food.price}VND
+            </p>
+          </div>
+        </div>
       ))}
       {foods && foods.length === 0 && (
         <div className="flex justify-center foods-center w-full h-full">
--- src/app/home/RestaurantList.tsx
+++ src/app/home/RestaurantList.tsx
@@ -21,32 +21,32 @@ const RestaurantList = ({ restaurantsData }: { restaurantsData: any }) => {
   };
 
   if (!restaurants || restaurants.length === 0) {
-    return <p>No restaurants found.</p>;
+    return <p>レストランが見つかりませんでした.</p>;
   }
 
   return (
     <div className="flex overflow-x-scroll pt-[10px] pb-[20px]">
       {restaurants.map((restaurant) => (
         <div
-        className="ml-4 mt-4 relative hover:scale-105 transition-all duration-300 flex-[1_0_20%] "
-        key={restaurant.id}
-        onClick={() => navigateToRestaurantDetail(restaurant.id)}
-      >
-        <div className="absolute top-5 left-5 bg-[#FF903F] text-white font-bold text-xs p-2 z-20 rounded ">
-          <StarFilled /> {Number.parseFloat(restaurant.rating).toFixed(1)}
-        </div>
-        <div className="p-[10px] h-[210px] max-w-[200px] text-gray-700 transition-shadow duration-300 shadow-sm bg-white relative mx-auto  overflow-hidden  w-full cursor-pointer rounded-md border border-orange-200 border-solid">
-          {/* Nội dung */}
-          <div className="h-[170px] w-full overflow-hidden">
-            <img
-              src={!restaurant.photoUrl? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : restaurant.photoUrl}
-              alt="food"
-              className="w-full h-fit"
-            />
+          className="ml-4 mt-4 relative hover:scale-105 transition-all duration-300 flex-[1_0_20%] "
+          key={restaurant.id}
+          onClick={() => navigateToRestaurantDetail(restaurant.id)}
+        >
+          <div className="absolute top-5 left-5 bg-[#FF903F] text-white font-bold text-xs p-2 z-20 rounded ">
+            <StarFilled /> {Number.parseFloat(restaurant.rating).toFixed(1)}
           </div>
-          <h5 className="font-bold my-2">{restaurant.name}</h5>
+          <div className="p-[10px] h-[210px] max-w-[200px] text-gray-700 transition-shadow duration-300 shadow-sm bg-white relative mx-auto  overflow-hidden  w-full cursor-pointer rounded-md border border-orange-200 border-solid">
+            {/* Nội dung */}
+            <div className="h-[170px] w-full overflow-hidden">
+              <img
+                src={!restaurant.photoUrl ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : restaurant.photoUrl}
+                alt="food"
+                className="w-full h-fit"
+              />
+            </div>
+            <h5 className="font-bold my-2">{restaurant.name}</h5>
+          </div>
         </div>
-      </div>
       ))}
       {restaurants?.length === 0 && (
         <div className="flex justify-center restaurants-center w-full h-full">
