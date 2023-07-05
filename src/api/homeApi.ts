35a8fd315b1ee94d03089428f0af15282fe5727b
kien2572001 <kien2572001.ntk@gmail.com>
2023-07-05 22:46:07 +0700

    fix: fix home api

--- src/api/homeApi.ts
+++ src/api/homeApi.ts
@@ -1,11 +1,12 @@
-import axios from "@/api/axiosJWT";
+import axios from "axios";
 import { Restaurant, Food } from "../models/home";
-
 
 const homeApi = {
   getRestaurantsAll: async (): Promise<{ restaurants: Restaurant[] }> => {
     try {
-      const res = await axios.get(`http://localhost:3008/api/restaurants/all`);
+      const res = await axios.get(
+        `http://13.212.172.169:3008/api/restaurants/all`
+      );
       return res.data;
     } catch (err) {
       throw err;
@@ -13,25 +14,31 @@ const homeApi = {
   },
   getFoodAll: async (): Promise<{ foods: Food[] }> => {
     try {
-      const response = await axios.get(`http://localhost:3008/api/foods/all`);
+      const response = await axios.get(
+        `http://13.212.172.169:3008/api/foods/all`
+      );
       return response.data;
     } catch (error) {
       throw error;
     }
   },
-  searchByKeyword: async (keyword: string): Promise<{ restaurant: Restaurant[]; food: Food[] }> => {
+  searchByKeyword: async (
+    keyword: string
+  ): Promise<{ restaurant: Restaurant[]; food: Food[] }> => {
     try {
-      const response = await axios.get(`http://localhost:3008/api/search`, {
-        params: {
-          keyword: keyword
+      const response = await axios.get(
+        `http://13.212.172.169:3008/api/search`,
+        {
+          params: {
+            keyword: keyword,
+          },
         }
-      });
+      );
       return response.data;
     } catch (error) {
       throw error;
     }
-  }
-
+  },
 };
 
 export default homeApi;
